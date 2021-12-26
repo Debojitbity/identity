4668c7b44d01c4c648b606a8378072b9fae3822c
Debojit Das <debojitdasbity@gmail.com>
2021-12-26 11:14:31 +0530

    init

---
+++ pages/guides.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Guides.module.css'
+
+export default function Guides() {
+  return (
+    <div className={styles.guides}>
+      <h2>Secret code is
+        <span className={styles.code}>
+          25256548
+        </span>
+      </h2>
+    </div>
+  )
+}
